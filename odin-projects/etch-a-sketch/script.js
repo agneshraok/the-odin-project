469486c1c67b761bd772603807bd7124bb0e541b
agneshraok <agneshraok.apps@gmail.com>
2023-04-26 15:15:41 +0530

    grid formation is now completed

--- odin-projects/etch-a-sketch/script.js
+++ odin-projects/etch-a-sketch/script.js
@@ -1,27 +1,47 @@
 //DOM elements
 
-const body = document.querySelector('body');
-const container = document.querySelector('#container');
-const sketchpad = document.querySelector('#sketchpad');
+const body = document.querySelector("body");
+const container = document.querySelector("#container");
+const sketchpad = document.querySelector("#sketchpad");
 
-const btn = document.querySelector('button');
- 
+const btn = document.querySelector("#set-size");
+
 //logic
-let size;
+let numCells;
 
+function cleanSketchPad() {
+  //clean the sketch pad (prev history)
+  sketchpad.innerHTML = "";
+}
 
-btn.addEventListener('click', ()=>{
-    size = prompt('Enter the number of cells:');
-    for(let i=0; i<=size; i++){
-        const row = document.createElement('div');
-        row.setAttribute('class', 'row');
-        row.setAttribute('style', "display: flex; flex:1;");
-        sketchpad.appendChild(row);
-        for(let j =0; j <= size; j++){
-            const cell = document.createElement('div');
-            cell.setAttribute('class', 'cell');
-            cell.setAttribute('style', 'flex: 1;');
-            row.appendChild(cell);
-        }
+btn.addEventListener("click", () => {
+  cleanSketchPad();
+  //take input from user, check for max condition
+  numCells = prompt("Enter the number of cells:");
+
+  if (numCells >= 0 && numCells <= 100) {
+    numCells = numCells;
+  } else if (numCells > 100) {
+    numCells = 100;
+  }
+
+  //calculate the width and height of each cell
+  const sizeOfSketchPad = window.getComputedStyle(
+    document.querySelector("#sketchpad")
+  ).height;
+  const sizeOfCell = sizeOfSketchPad / numCells;
+
+  //draw the cells
+  for (let i = 0; i < numCells; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("style", `height: ${sizeOfCell}`);
+    sketchpad.appendChild(row);
+    for (let j = 0; j < numCells; j++) {
+      const cell = document.createElement("div");
+      cell.setAttribute("class", "cell");
+      cell.setAttribute("style", `width:${sizeOfCell}`);
+      row.appendChild(cell);
     }
-})
+  }
+});
---
+++ odin-projects/etch-a-sketch/test.js
@@ -0,0 +1,3 @@
+const div = document.querySelector("#test");
+const res = window.getComputedStyle(document.querySelector("#test")).height;
+console.log(res);
