2b229cae9743d848057c21b319c493e90ab80dd4
agneshraok <agneshraok.apps@gmail.com>
2023-04-26 23:31:50 +0530

    logic is worked out, improve css

--- odin-projects/library/prod/script.js
+++ odin-projects/library/prod/script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector("#new-book");
-
+const buttonContainer = document.querySelector("#button-container");
+const container = document.querySelector("#container");
 let myLibrary = [];
 
 function Book(title, author, pages) {
@@ -8,14 +9,41 @@ function Book(title, author, pages) {
   this.pages = pages;
 }
 
+function displayBook() {
+  container.innerHTML = "";
+
+  for (const book of myLibrary) {
+    const card = document.createElement("div");
+    card.setAttribute("id", `book-${myLibrary.indexOf(book)}`);
+    card.setAttribute(
+      "style",
+      "height: 300px; width: 300px; border-radius: 10px"
+    );
+
+    card.innerHTML = `<div>${book.title}</div><div>${book.author}</div><div>${book.pages}</div>`;
+
+    container.appendChild(card);
+  }
+}
+
 function addBookToLibrary() {
   const title = prompt("Enter the title");
   const author = prompt("Enter the author");
   const pages = prompt("Enter the pages");
 
-  const usersBook = new Book(title, author, pages);
-  myLibrary.push(usersBook);
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
   console.log(myLibrary);
+
+  displayBook();
 }
 
 button.addEventListener("click", addBookToLibrary);
+
+//change background color on mouse hover over add book button
+button.addEventListener("mouseenter", () => {
+  button.setAttribute("style", "background-color: greenyellow");
+});
+buttonContainer.addEventListener("mouseleave", () =>
+  button.setAttribute("style", "background-color: white")
+);
